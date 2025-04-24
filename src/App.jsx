e22965829a62fad18d1a30215309e0c869494bb1
Luka <luka.luka.luka93@gmail.com>
2025-04-23 17:18:47 -0700

    made function that will handle adding items to cart

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,37 @@ import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+
+  function handleAddItemToCart(id){
+    setShoppingCart(prevCart => {
+      const updatedItems = [...prevCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(item => item.id === id);
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if(existingCartItem){
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find(product => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        })
+      };
+      return {
+        items: updatedItems
+      }
+    })
+  }
 
 
 
